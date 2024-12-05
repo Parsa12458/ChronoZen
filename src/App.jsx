4f1c3afaf22263ca59f7ccf39ef7c3fed9f5de06
Parsa12458 <parsa.shirafkan127@gmail.com>
2024-12-05 11:48:12 +0330

    implement login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
 import AppLayout from "./ui/AppLayout";
 import PageNotFound from "./ui/PageNotFound";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route element={<AppLayout />}>
           <Route path="/" element={<Navigate to="/signup" />} index />
           <Route path="*" element={<PageNotFound />} />
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,28 +3,22 @@ import Button from "../ui/Button";
 import Header from "../ui/Header";
 import InputField from "../ui/InputField";
 
-function SignupPage() {
+function LoginPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <div className="flex flex-grow items-center justify-center bg-background py-16">
         <div className="w-96 bg-lightGreen p-9">
-          <h2 className="mb-6 text-center text-3xl font-semibold">Signup</h2>
+          <h2 className="mb-6 text-center text-3xl font-semibold">Login</h2>
           <button className="flex w-full items-center justify-center gap-2 rounded border border-mediumGreen py-1.5 font-semibold">
             <img
               src="/icons/google-icon.svg"
               alt="google icon"
               className="w-6"
             />
-            <span>Signup with Google</span>
+            <span>Login with Google</span>
           </button>
           <form className="mt-7 flex flex-col items-stretch space-y-5">
-            <InputField
-              id="name"
-              label="Full Name"
-              placeHolder="Enter your full name"
-              type="text"
-            />
             <InputField
               id="email"
               label="Email Address"
@@ -41,10 +35,14 @@ function SignupPage() {
               Start your journey!
             </Button>
           </form>
+
           <span className="mt-2.5 inline-block text-sm">
-            Already have an account?{" "}
-            <Link className="underline" to="/login">
-              Login
+            Forget password? <Link className="underline">Reset password</Link>
+          </span>
+          <span className="inline-block text-sm">
+            Don't have an account?{" "}
+            <Link className="underline" to="/signup">
+              Signup
             </Link>
           </span>
         </div>
@@ -53,4 +51,4 @@ function SignupPage() {
   );
 }
 
-export default SignupPage;
+export default LoginPage;
