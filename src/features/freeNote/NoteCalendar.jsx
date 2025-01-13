70a006f655b68ef117695489142c4929ee0c5124
Parsa12458 <parsa.shirafkan127@gmail.com>
2025-01-13 19:03:43 +0330

    add calendar to daily free-note page

---
+++ src/features/freeNote/NoteCalendar.jsx
@@ -0,0 +1,7 @@
+import Calendar from "react-calendar";
+
+function NoteCalendar() {
+  return <Calendar calendarType="islamic" className="habit-calendar" />;
+}
+
+export default NoteCalendar;
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,14 +1,20 @@
+import NoteCalendar from "../features/freeNote/NoteCalendar";
 import TextEditor from "../features/freeNote/TextEditor";
 
 function NotesPage() {
   return (
     <div>
       <h1 className="mb-8 text-2xl font-bold">Daily Free-Note</h1>
-      <div className="w-2/3">
-        <span className="mb-1 inline-block text-sm font-bold">
-          Today Free-note:
-        </span>
-        <TextEditor />
+      <div className="flex gap-10">
+        <div className="w-full">
+          <span className="mb-1 inline-block text-sm font-bold">
+            Today Free-note:
+          </span>
+          <TextEditor />
+        </div>
+        <div className="mt-6 w-full max-w-80">
+          <NoteCalendar />
+        </div>
       </div>
     </div>
   );
