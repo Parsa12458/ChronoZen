ce7eb311a6d26fab9d3ee4f409adce1e84be5209
Parsa12458 <parsa.shirafkan127@gmail.com>
2025-04-03 17:01:13 +0330

    implement code splitting for optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import SignupPage from "./pages/SignupPage";
-import AppLayout from "./ui/AppLayout";
-import PageNotFound from "./ui/PageNotFound";
-import LoginPage from "./pages/LoginPage";
-import DashboardPage from "./pages/DashboardPage";
-import TasksPage from "./pages/TasksPage";
-import HabitsPage from "./pages/HabitsPage";
-import EventsPage from "./pages/EventsPage";
-import GoalsPage from "./pages/GoalsPage";
-import NotesPage from "./pages/NotesPage";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ProtectedRoute from "./ui/ProtectedRoute";
 import { ModalProvider } from "./contexts/ModalContext";
+import FullSpinner from "./ui/FullSpinner";
+const SignupPage = lazy(() => import("./pages/SignupPage"));
+const AppLayout = lazy(() => import("./ui/AppLayout"));
+const PageNotFound = lazy(() => import("./ui/PageNotFound"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const DashboardPage = lazy(() => import("./pages/DashboardPage"));
+const TasksPage = lazy(() => import("./pages/TasksPage"));
+const HabitsPage = lazy(() => import("./pages/HabitsPage"));
+const EventsPage = lazy(() => import("./pages/EventsPage"));
+const GoalsPage = lazy(() => import("./pages/GoalsPage"));
+const NotesPage = lazy(() => import("./pages/NotesPage"));
 
 export const queryClient = new QueryClient();
 
@@ -31,41 +33,43 @@ function App() {
               className: "!px-4 font-medium !max-w-full",
             }}
           />
-          <Routes>
-            <Route
-              path="/signup"
-              element={
-                <ProtectedRoute navigateUrl="/dashboard">
-                  <SignupPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <ProtectedRoute navigateUrl="/dashboard">
-                  <LoginPage />
-                </ProtectedRoute>
-              }
-            />
+          <Suspense fallback={<FullSpinner />}>
+            <Routes>
+              <Route
+                path="/signup"
+                element={
+                  <ProtectedRoute navigateUrl="/dashboard">
+                    <SignupPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <ProtectedRoute navigateUrl="/dashboard">
+                    <LoginPage />
+                  </ProtectedRoute>
+                }
+              />
 
-            <Route
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="dashboard" />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/tasks" element={<TasksPage />} />
-              <Route path="/habits" element={<HabitsPage />} />
-              <Route path="/events" element={<EventsPage />} />
-              <Route path="/goals" element={<GoalsPage />} />
-              <Route path="/notes" element={<NotesPage />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+              <Route
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="dashboard" />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/tasks" element={<TasksPage />} />
+                <Route path="/habits" element={<HabitsPage />} />
+                <Route path="/events" element={<EventsPage />} />
+                <Route path="/goals" element={<GoalsPage />} />
+                <Route path="/notes" element={<NotesPage />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </ModalProvider>
       </QueryClientProvider>
     </BrowserRouter>
